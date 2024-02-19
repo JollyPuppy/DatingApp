a12142f50516a4aa1daf6b323cb11c8a0ea4f04d
JollyPuppy <ghurd2002@hotmail.com>
2024-02-19 11:30:17 +0200

    Added auth basics(user,pw,jwt,etc..)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,15 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+
+[Authorize]
+public class UsersController : BaseApiController
 {
+    
     private readonly DataContext _context;
 
     public UsersController(DataContext context)
@@ -15,12 +17,15 @@ public class UsersController : ControllerBase
         _context = context;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await _context.Users.ToListAsync();
         return users;
     }
+
+
 
     [HttpGet("{id}")] // /api/users/2
     public async Task<ActionResult<AppUser>> GetUser(int id)
